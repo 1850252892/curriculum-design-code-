2d410b34776efaa7a00b842471c1ec67599b8eaa
1850252892@qq.com <1850252892@qq.com>
2018-12-04 16:20:26 +0800

    完成注册模块，整合QQ登陆

--- serice-feign/src/main/java/com/shop/sericefeign/SchedualServiceHiHystric.java
+++ serice-feign/src/main/java/com/shop/sericefeign/SchedualServiceHiHystric.java
@@ -2,7 +2,7 @@ package com.shop.sericefeign;
 
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class SchedualServiceHiHystric implements SchedualServiceHi {
     @Override
     public String sayHiFromClientOne(String name) {
